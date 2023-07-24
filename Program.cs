bdfd788a730145b4d8f2b9d0b935a6e427a02605
WhatIsFun <mohammed110051@gmail.com>
2023-07-24 11:07:19 +0400

    cSharp-LambaExpressions

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace cSharp_LambaExpressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation mathOperation = Add;
+            int result = mathOperation(5, 3);
+            mathOperation = Multiply;
+            int result1 = mathOperation(6, 6);
+            Console.WriteLine(result);
+            Console.WriteLine(result1);
+        }
+        public delegate int MathOperation(int a, int b);
+        static int Add(int a, int b) => a + b;
+        static int Multiply(int a, int b) => a * b;
+
+    }
+
+    
+}
